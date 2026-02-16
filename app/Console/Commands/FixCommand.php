fff4faf3ad617e83da147138e919354597ea64d6
Senechal jf <jfsenechal@gmail.com>
2026-02-16 09:59:48 +0100

    Add FixCommand to fetch and display citizen data from LdapCitoyenRepository

---
+++ app/Console/Commands/FixCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Ldap\LdapCitoyenRepository;
+use Illuminate\Console\Command;
+
+class FixCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'citoyen:fix';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Recherche un compte citoyen suivant le mot clef';
+
+    public function __construct(private readonly LdapCitoyenRepository $ldapCitoyenRepository)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+            try {
+                $citizens = $this->ldapCitoyenRepository->getAll();
+            } catch (\Exception $e) {
+                $this->error($e->getMessage());
+
+                return \Symfony\Component\Console\Command\Command::FAILURE;
+            }
+
+            $this->line('Found '.count($citizens));
+            foreach ($citizens as $citizen) {
+                dd($citizen);
+                $this->line($citizen->getFirstAttribute('mail'));
+            }
+
+        return \Symfony\Component\Console\Command\Command::SUCCESS;
+    }
+}
