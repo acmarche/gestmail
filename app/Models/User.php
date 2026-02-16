24859d57bd9aefc22673c53e5247aa7dfe379683
Senechal jf <jfsenechal@gmail.com>
2026-02-16 12:14:06 +0100

    Refactor Models: update `Hand` fillable attributes, simplify `User` model, and add `Login` model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,23 +1,27 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Filament\Auth\MultiFactor\App\Contracts\HasAppAuthentication;
-use Filament\Auth\MultiFactor\App\Contracts\HasAppAuthenticationRecovery;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-final class User extends Authenticatable implements FilamentUser, HasAppAuthentication, HasAppAuthenticationRecovery
+class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -27,45 +31,7 @@ final class User extends Authenticatable implements FilamentUser, HasAppAuthenti
     protected $hidden = [
         'password',
         'remember_token',
-        'app_authentication_secret',
-        'app_authentication_recovery_codes',
     ];
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        /* TODO: Please implement your own logic here. */
-        return true; // str_ends_with($this->email, '@larament.test');
-    }
-
-    public function getAppAuthenticationSecret(): ?string
-    {
-        return $this->app_authentication_secret;
-    }
-
-    public function saveAppAuthenticationSecret(?string $secret): void
-    {
-        $this->app_authentication_secret = $secret;
-        $this->save();
-    }
-
-    public function getAppAuthenticationHolderName(): string
-    {
-        return $this->email;
-    }
-
-    /** @phpstan-ignore-next-line */
-    public function getAppAuthenticationRecoveryCodes(): ?array
-    {
-        /** @phpstan-ignore-next-line */
-        return $this->app_authentication_recovery_codes;
-    }
-
-    public function saveAppAuthenticationRecoveryCodes(?array $codes): void
-    {
-        /** @phpstan-ignore-next-line  */
-        $this->app_authentication_recovery_codes = $codes;
-        $this->save();
-    }
 
     /**
      * Get the attributes that should be cast.
@@ -77,8 +43,6 @@ final class User extends Authenticatable implements FilamentUser, HasAppAuthenti
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'app_authentication_secret' => 'encrypted',
-            'app_authentication_recovery_codes' => 'encrypted:array',
         ];
     }
 }
