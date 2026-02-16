45bf3c66e0f94c6bd78800c7190de2e83075690a
Senechal jf <jfsenechal@gmail.com>
2026-02-16 17:31:41 +0100

    Update LDAP configuration: add default hosts, base_dn, and connection settings

--- config/ldap.php
+++ config/ldap.php
@@ -1,5 +1,13 @@
 <?php
 
   return [
-      'sieve_root' => env('SIEVE_ROOT'),
+    'hosts' => env('LDAP_DEFAULT_HOSTS', '127.0.0.1'),
+    'base_dn' => env('LDAP_DEFAULT_BASE_DN', 'dc=local,dc=com'),
+    'username' => env('LDAP_DEFAULT_USERNAME'),
+    'password' => env('LDAP_DEFAULT_PASSWORD'),
+    'port' => env('LDAP_DEFAULT_PORT', 389),
+    'ssl' => env('LDAP_DEFAULT_SSL', false),
+    'tls' => env('LDAP_DEFAULT_TLS', false),
+    'sasl' => env('LDAP_DEFAULT_SASL', false),
+    'timeout' => env('LDAP_DEFAULT_TIMEOUT', 5),
   ];
