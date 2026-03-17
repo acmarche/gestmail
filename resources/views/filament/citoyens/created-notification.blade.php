4b8c45ca987470f25955fdd00f113ec6f04d2278
Senechal jf <jfsenechal@gmail.com>
2026-03-17 12:04:08 +0100

    update(created-notification): adjust URLs and improve formatting for clarity

--- resources/views/filament/citoyens/created-notification.blade.php
+++ resources/views/filament/citoyens/created-notification.blade.php
@@ -2,14 +2,18 @@ Bonjour,
 
 L'adresse **{{ $mail }}** a bien été créée.
 
-Vous pouvez la consulter sur https://www.marche.be
-
-Vous pouvez configurer votre mail marche.be dans un logiciel de messagerie en vous aidant du tutoriel sur la page : https://www.marche.be/email/
+Vous pouvez la consulter sur https://citoyen.marche.be
 
 Le nom d'utilisateur est : **{{ $uid }}**
+
 Le mot de passe est : **{{ $password }}**
 
+Vous pouvez configurer votre mail marche.be dans un logiciel de messagerie en vous aidant du tutoriel sur la page :
+
+https://www.marche.be/email/
+
 Votre mot de passe peut être changé sur https://citoyen.marche.be.
+
 Lorsque vous êtes connecté, cliquez sur "Paramètres" puis "Mot de passe"
 
 Bien à vous,
